847a64c411b056415cf4f74ab21b401b4ac2153c
Piotr Piech <tenemo@gmail.com>
2023-03-27 21:42:00 +0200

    sentry integration, packages update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import Root from 'components/Root';
+
+Sentry.init({
+    dsn: 'https://ae9f630bfc884a3a86d5a3bbb5ae29c4@o502294.ingest.sentry.io/4504912262463488',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById('root');
 
