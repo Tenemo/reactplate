3bad2436c6446330517f33d5255d7cdb699b549e
Tenemo <tenemo@gmail.com>
2025-03-27 18:01:27 +0100

    checking for app title in header tests

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,18 +1,33 @@
 import { screen, act } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 
 import { makeStore } from 'app/store';
 import Header from 'components/Header/Header';
 import { renderWithProviders } from 'utils/test-utils';
 
 describe('Header', () => {
-    it('clicking theme toggle button should change the theme', async () => {
-        const store = makeStore({
+    let store: ReturnType<typeof makeStore>;
+
+    beforeEach(() => {
+        store = makeStore({
             appTheme: { theme: 'dark' },
         });
+    });
 
+    it('should display the app title', () => {
+        renderWithProviders(
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>,
+            { store },
+        );
+        const title = screen.getByText('reactplate');
+        expect(title).not.toBeNull();
+    });
+
+    it('clicking theme toggle button should change the theme', async () => {
         const { user } = renderWithProviders(
             <BrowserRouter>
                 <Header />
