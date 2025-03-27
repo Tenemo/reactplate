2c4af3cd90fbd3fc025d51f3a32355bade9e2c1e
Tenemo <tenemo@gmail.com>
2025-03-27 19:01:34 +0100

    largest contentful paint improved by rendering the heading during loading

--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -10,9 +10,7 @@ import {
 const HomePage = (): React.JSX.Element => {
     const { data, error, isLoading } = useGetExamplePageQuery();
 
-    if (isLoading) return <div>Loading...</div>;
     if (error) return <div>An error occurred</div>;
-    if (!data) return <div>No data found</div>;
     return (
         <main>
             <h2>
@@ -25,19 +23,22 @@ const HomePage = (): React.JSX.Element => {
                     spaceflightnewsapi.net
                 </a>
             </h2>
-            <ul className={styles.linksList}>
-                {data.results.map((article: Article) => (
-                    <li key={article.id}>
-                        <a
-                            href={article.url}
-                            rel="noopener noreferrer"
-                            target="_blank"
-                        >
-                            {article.title}
-                        </a>
-                    </li>
-                ))}
-            </ul>
+            {isLoading && <div>Loading...</div>}
+            {!isLoading && (
+                <ul className={styles.linksList}>
+                    {data?.results.map((article: Article) => (
+                        <li key={article.id}>
+                            <a
+                                href={article.url}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                {article.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </main>
     );
 };
