5b53c236bd55a0c25789db71721945a01f5a3f5b
Tenemo <tenemo@gmail.com>
2025-03-29 14:00:20 +0100

    main.tsx excluded from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
                 'vite.config.ts',
                 'vitest.config.ts',
                 'dist/*',
+                'src/main.tsx',
             ],
         },
     },
