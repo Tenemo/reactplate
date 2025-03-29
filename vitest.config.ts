a660109c07003ea9cd106e80f5a261d03021eb5a
Tenemo <tenemo@gmail.com>
2025-03-29 13:13:32 +0100

    coverage excluding files not meant to be tested

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,15 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             reporter: ['text', 'html'],
-            exclude: ['temp/*'],
+            exclude: [
+                'temp/*',
+                '**/*.d.ts',
+                'eslint.config.js',
+                'postcss.config.js',
+                'vite.config.ts',
+                'vitest.config.ts',
+                'dist/*',
+            ],
         },
     },
 });
